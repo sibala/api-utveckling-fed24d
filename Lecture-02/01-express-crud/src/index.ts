094e92217c78e630d025c6dfe5de9af18286ad6b
Sibar Al-ani <sibar.alani@gmail.com>
2025-03-20 09:39:26 +0100

    Post request done

---
+++ Lecture-02/01-express-crud/src/index.ts
@@ -0,0 +1,99 @@
+import express, { Request, Response } from 'express';
+import { Todo } from './models/Todo';
+const app = express();
+
+
+// Define root path with message
+// req, handles the incoming request from the client
+// res, handles the outgoing response back ti the client
+// May use _ to indicate that the parameter is not used
+app.get('/', (_: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+const todos: Todo[] = [
+  new Todo('AAA'),
+  new Todo('BBB'),
+  new Todo('CCC'),
+  new Todo('Handla mat'),
+  new Todo('Käka mat'),
+  new Todo('Diska'),
+  new Todo('Diska'),
+]
+
+// Example query string params
+app.get('/todos', (req: Request, res: Response) => {
+  const search = req.query.search
+  const sort = req.query.sort
+
+  let filteredTodos = todos;
+
+  if (search) {
+    filteredTodos = filteredTodos.filter((t) => t.content.includes(search.toString()))
+  }
+  
+  if (sort && sort === "asc") {
+    filteredTodos = filteredTodos.sort((a, b) => {
+      const todo1 = a.content.toLowerCase()
+      const todo2 = b.content.toLowerCase()
+
+      if (todo1 > todo2) return 1
+      if (todo1 < todo2) return -1
+      return 0
+    })
+  }
+
+  if (sort && sort === "desc") {
+    filteredTodos = filteredTodos.sort((a, b) => {
+      const todo1 = a.content.toLowerCase()
+      const todo2 = b.content.toLowerCase()
+
+      if (todo1 < todo2) return 1
+      if (todo1 > todo2) return -1
+      return 0
+    })
+  }
+
+  
+  res.json(filteredTodos)
+})
+
+
+// Example path params
+app.get('/todos/:id', (req: Request, res: Response) => {
+  const id = req.params.id
+  const todo = todos.find((t) => t.id === parseInt(id))
+
+  res.json({todo})
+})
+
+
+// General Middleware for all requests
+app.use(express.json()); // This specific middleware parses JSON string to Javascript Object
+
+
+// Create todo
+app.post('/todos', (req: Request, res: Response) => {
+  const content = req.body.content;
+  if (content === undefined) {
+    res.json({error: 'Content is required'}) 
+    return; 
+  }
+  // const todoExists = todos.find((t) => t.content === content)
+  // if (todoExists) {
+  //   res.json({message: 'Todo exisits already. Please create antoher todo'})  
+  //   return;
+  // }
+  // console.log(todoExists)
+
+  const newTodo = new Todo(content) // content = "Släng soporna"
+  todos.push(newTodo);
+  
+  res.status(201).json({message: 'Todo created', data: newTodo})
+})
+
+
+const PORT = 3000
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
--- Lecture-02/README.md
+++ Lecture-02/README.md
@@ -1,1 +1,12 @@
-# Coming soon
+# Exercises
+
+## Exercise 01-express-crud
+- Build on the previous code, create new endpoints for POST/PATCH/DELETE posts 
+- The POST endpoint should create a new post with the following properties: id, title, content, author
+  - Validate required fields title, content, author
+
+
+## Exercises for later
+- The PATCH endpoint should update an existing post with the “:id” path param. Should be able to update the title, content and author
+  - Validate required fields title, content, author
+- The DELETE endpoint should delete an existing post with the “:id” path param
