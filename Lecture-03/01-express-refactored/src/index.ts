5a2ba97243c18b58d814241fa722d2e1387fd29c
Sibar Al-ani <sibar.alani@gmail.com>
2025-03-21 14:49:21 +0100

    Client example with async/await try/catch

--- Lecture-03/01-express-refactored/src/index.ts
+++ Lecture-03/01-express-refactored/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors'
 const app = express();
+
 
 // Middleware
 app.use(express.json()); // This specific middleware parses JSON string to Javascript Object
+app.use(cors());        // This makes the Express server except request from other domains
 
 
 // Routes
