9ecbd9b1eccb9d32370472f50d95564f0ca07d10
Sibar Al-ani <sibar.alani@gmail.com>
2025-05-12 17:48:08 +0200

    credentials for managing cookies

--- Lecture-17/todo-app/todo-client/src/views/AddTodoView.vue
+++ Lecture-17/todo-app/todo-client/src/views/AddTodoView.vue
@@ -14,6 +14,7 @@
     try {
       await fetch(API_URL + '/todos', {
         method: "POST",
+        credentials: 'include',
         headers: {
           "Content-Type": "application/json"
         },
